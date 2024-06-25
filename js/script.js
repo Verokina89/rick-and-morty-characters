e9570ae9bf24cba7b4842ae763bed7bf0437bbdc
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-06-25 23:58:45 +0200

    codigo a mitad con css,hasta los botones;se rompe,pendiente arreglar

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// traer con fetch los datos de la API. https://rickandmortyapi.com/api/character/?page=1. ---> ok
+//capturar el espacio en el html para poner los personajes. ---> ok
+//crear un html dentro con un bucle para traer los 20 personajes. ---> ok
+//poner las css para que esten uno al lado del otro.
+
+//hacer funcionar los botones.
+
+const urlHomepage = "https://rickandmortyapi.com/api/character/?page="
+
+let page = 1
+
+const characterList = document.getElementById("character-list")
+const prevButton = document.getElementById("prevPage")
+const nextButton = document.getElementById("nextPage")
+
+const getCharacters = () => {
+    //characterList.innerHTML = ""
+    fetch(`${urlHomepage}${page}`)
+    .then(response => response.json())
+    .then((data) => {
+        let characters = data.results;
+        characters.forEach((character) => {
+            const template = 
+            `
+            <li>
+            <img src=${character.image} alt=${character.name} />
+            <h2><span>Name:</span> ${character.name}</h2>
+            <p><span>Spacies:</span> ${character.species}</p>
+            </li>
+            `
+            characterList.innerHTML += template
+        });
+    })   
+} 
+
+/*
+prevButton.addEventListener("click",() => {
+    page--
+    getCharacters()
+})
+
+nextButton.addEventListener("click", () => {
+    page++
+    getCharacters()
+})
+
+getCharacters()
+
+*/
+
+/*------> otro metodo
+
+
+const changePage = (clickValue) => {
+    click value
+    getCharacters()
+}
+
+---->otra cosuca que no estaba en mi mente
+<---- antes va characterList.HTML   
+
+if (page === 1) {
+prevBotton.disabled = true
+prevBotton.classList.add("disabled")
+} else {
+ }   
+
+ ---->despues va el fetch
+*/
